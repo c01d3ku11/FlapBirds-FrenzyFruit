dc5b0ea06d939589cccf6fe7694673ae8502c535
c01d3ku11 <69782909+c01d3ku11@users.noreply.github.com>
2021-04-13 17:55:21 +1000

    make the bird flap

---
+++ Flappy Bird/flappyBird/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public Rigidbody2D myRigid;
+public float flapStrength;
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)){
+            myRigid.velocity = Vector2.up * flapStrength;
+        }
+    }
+}
