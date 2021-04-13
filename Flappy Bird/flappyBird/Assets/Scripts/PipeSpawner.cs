24372f43cdbad008c9fd545c70e26bfaeceef2b5
c01d3ku11 <69782909+c01d3ku11@users.noreply.github.com>
2021-04-13 18:10:22 +1000

    added pipe spawner script

---
+++ Flappy Bird/flappyBird/Assets/Scripts/PipeSpawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeSpawner : MonoBehaviour
+{
+ public GameObject pipe;
+public float spawnTime;
+public float startingX;
+public float minY;
+public float maxY;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnPipe();
+    }
+void SpawnPipe(){
+    GameObject.Instantiate(pipe, new Vector3(startingX, Random.Range(minY, maxY), 0), Quaternion.identity);
+        Invoke("SpawnPipe", spawnTime);
+}
+
+}
