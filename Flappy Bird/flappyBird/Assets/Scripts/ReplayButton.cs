e567abe7e3de385609aa11c1fc6c7a9171f6654b
c01d3ku11 <69782909+c01d3ku11@users.noreply.github.com>
2021-04-14 01:57:40 +1000

    finished base game

--- Flappy Bird/flappyBird/Assets/Scripts/ReplayButton.cs
+++ Flappy Bird/flappyBird/Assets/Scripts/ReplayButton.cs
@@ -1,18 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ReplayButton : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+   void OnMouseDown() 
     {
-        
+       SceneManager.LoadScene("gameScene"); 
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  
 }
