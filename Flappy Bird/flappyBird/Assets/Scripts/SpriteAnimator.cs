16c43fc14dd94cd5fd04065f7937b0a7df532c91
c01d3ku11 <69782909+c01d3ku11@users.noreply.github.com>
2021-04-14 02:39:11 +1000

    added sprite animator script

---
+++ Flappy Bird/flappyBird/Assets/Scripts/SpriteAnimator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteAnimator : MonoBehaviour
+{
+    public Sprite[] mySprites;
+    public SpriteRenderer mySpriteRenderer;
+public float framesPerSecond;
+public float secondsPerFrame;
+public int currentFrame;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+  void NextFrame()
+  {
+      currentFrame = (currentFrame + 1)% mySprites.Length;
+      mySpriteRenderer.sprite = mySprites[currentFrame];
+      Invoke("NextFrame", secondsPerFrame);
+  }
+}
