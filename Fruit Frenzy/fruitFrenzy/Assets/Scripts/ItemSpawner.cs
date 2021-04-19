fee95017e54fcc2762089b986e50b312904639bd
c01d3ku11 <69782909+c01d3ku11@users.noreply.github.com>
2021-04-19 18:55:32 +1000

    spawned objects successful

---
+++ Fruit Frenzy/fruitFrenzy/Assets/Scripts/ItemSpawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSpawner : MonoBehaviour
+{
+    public GameObject[] myItems;
+public int[] itemCounts;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int itemNo=0; itemNo<myItems.Length; itemNo++)
+        {
+            for (int count=0; count<itemCounts[itemNo]; count++)
+            {
+                GameObject.Instantiate(myItems[itemNo]);
+            }
+        }
+    }
+
+   
+}
