2f8ca1b7212c01507823e2221437c7d2f3ce1340
c01d3ku11 <69782909+c01d3ku11@users.noreply.github.com>
2021-04-19 18:22:04 +1000

    finished itemcontrol script

---
+++ Fruit Frenzy/fruitFrenzy/Assets/Scripts/ItemControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemControl : MonoBehaviour
+{
+    public Rigidbody2D myRigid;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigid = GetComponent<Rigidbody2D>();
+       Spawn();
+    }
+
+   void Spawn()
+   {
+       transform.position = new Vector3(Random.Range(-4,4), Random.Range(-4,4),0);
+       myRigid.velocity = Random.insideUnitCircle.normalized * speed;
+   }
+}
