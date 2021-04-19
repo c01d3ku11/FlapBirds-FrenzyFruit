8ef8cce57cdaac05fd440ef55f115b474664de08
c01d3ku11 <69782909+c01d3ku11@users.noreply.github.com>
2021-04-19 18:42:51 +1000

    added gamecontrol script

---
+++ Fruit Frenzy/fruitFrenzy/Assets/Scripts/GameControl.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameControl : MonoBehaviour
+{
+    public int score;
+    public Text scoreText;
+    // Start is called before the first frame update
+    void Start()
+    {
+       SetText();
+    }
+
+    void SetText()
+    {
+        scoreText.text = score.ToString();
+    }
+
+    public void AddPoints(int points)
+    {
+        score = score + points;
+        SetText();
+    }
+
+    public void GameOver()
+{
+//save the score
+//go to game over screen
+}
+
+    
+}
