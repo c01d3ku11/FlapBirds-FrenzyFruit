e73206f6490bc083ebba9242b04dcef53e9ef5cc
Jeffrey( Cheuk) Chung <13912201@student.uts.edu.au>
2022-02-22 20:25:51 +1100

    compared X coordinate of player to obstacles

--- Flappy Bird/flappyBird/Assets/Scripts/GameControl.cs
+++ Flappy Bird/flappyBird/Assets/Scripts/GameControl.cs
@@ -8,14 +8,23 @@ public class GameControl : MonoBehaviour
 {
     public float score;
     public Text myText;
+    public GameObject bird;
+    public GameObject pipe;
  
 
     // Update is called once per frame
     void Update()
     {
-        score = score + Time.deltaTime;
-        myText.text = score.ToString();
+       if((int)bird.transform.position.x==-(int)pipe.transform.position.x)
+       {
+       score++;
+       myText.text = score.ToString();
+       }
+        //score = score + Time.deltaTime;
+        //myText.text = score.ToString();
+    
     }
+   
     public void GameOver()
     {
         PlayerPrefs.SetFloat("playerScore", score);
